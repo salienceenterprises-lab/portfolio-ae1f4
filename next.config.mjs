9781f4e4851cf2a5db1e4aeef559ac7de3b4378d
Salience Enterprises <salienceenterprises@gmail.com>
2026-04-07 17:52:30 -0400

    Preview: next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: '/portfolio-ae1f4',
+};
+export default nextConfig;
